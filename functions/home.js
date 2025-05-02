c96c9958a24951f1fc5c09b6986f3c985cbf676b
Abdullahi <akintadeabdullahi@gmail.com>
2025-05-02 22:22:42 +0100

    second commit

--- functions/home.js
+++ functions/home.js
@@ -1,5 +1,24 @@
-const {addNumber} = require('./index')
+const {addNumber, multiplyNumber, typing} = require('./index')
+
 
 // const {} = require('./Algorithm/index.js')
 // MediaSourceHandle.exports = {addNumber}
-console.log(addNumber(2, 3))
+// console.log(addNumber(2, 3))
+function doSomething() {
+    const a = multiplyNumber(5, 8);
+    if(a >= 10){
+      console.log("Result is more than 10")  
+    }
+    else {
+        console.log("Result is less 10")
+    }
+  
+}
+
+// doSomething();
+function runSomething(){
+    typing()
+    console.log("i am doing my own")
+}
+
+runSomething()
--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,12 @@ function addNumber(a, b){
    return a+b;
 }
 
-module.exports = {addNumber};
+function multiplyNumber(a, b) {
+    return a*b
+}
+
+function typing(){
+    console.log("i am typing...")
+}
+
+module.exports = {addNumber, multiplyNumber,typing};
