39744e10a85781838d824fd8d8dafc89512329f6
Abdullahi <akintadeabdullahi@gmail.com>
2025-05-02 11:57:04 +0100

    second commit

---
+++ Algorithm/index.js
@@ -0,0 +1,31 @@
+//First we want to capitalize the first letter of a string
+
+const tocapitalize({
+    return tocapitalizeFirstLetter.charAt(0).toUpperCase + tocapitalizeFirstLetter.splice(1)
+})
+
+
+function tocapitalizeFirstLetterMap(){
+   
+    const newLetter = tocapitalizelizeFirstLetter.split("")
+    console.log(newLetter)
+    const result = newLetter.map((char, index) => {
+        if (index ===num) {
+            return char.
+        }
+    })
+}
+
+
+
+
+//reverse string temitope = epotimet
+const reverseString = "temitope"
+
+
+function reverseStringFunction(){
+    const newLetters = reverseString.split("")
+    return newLetters.reverse().join
+
+}
+reverseStringFunction()
---
+++ functions/home.js
@@ -0,0 +1,5 @@
+const {addNumber} = require('./index')
+
+// const {} = require('./Algorithm/index.js')
+// MediaSourceHandle.exports = {addNumber}
+console.log(addNumber(2, 3))
